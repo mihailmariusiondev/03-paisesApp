01d047f8b11d4ce9562801562a0e9af0a8007b4b
MariusMihailEnzo <marius.mihail@enzo.es>
2021-09-13 18:16:47 +0200

    100-router-module rutas en la app

---
+++ paisesApp/src/app/app-routing.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+
+
+const routes: Routes = [
+    {
+        path: '',
+        component: PorPaisComponent,
+        pathMatch: 'full' //que coincida la ruta entera
+    },
+    {
+        path: 'region',
+        component: PorRegionComponent,
+    },
+    {
+        path: 'capital',
+        component: PorCapitalComponent,
+    },
+    {
+        path: 'pais/:codigoPais',
+        component: VerPaisComponent,
+    },
+    {
+        // ruta 404 (no encontrado o no coincide ninguna ruta)
+        path: '**',
+        redirectTo: ''
+    },
+
+];
+
+@NgModule({
+    imports: [
+        // forRoot = rutas principales
+        // forChild = rutas secundarias
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {
+
+}
