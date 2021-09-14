7823e60e9c12fc8bf12350608553e9c25a72cbac
MariusMihailEnzo <marius.mihail@enzo.es>
2021-09-14 17:50:29 +0200

    114-terminar-pantalla-ver-pais

--- paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -11,18 +12,18 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country
+
   constructor(private activatedRoute: ActivatedRoute, private paisService: PaisService) { }
 
   ngOnInit(): void {
-
     this.activatedRoute.params
       .pipe(
-        switchMap(({ id }) => this.paisService.getPaisPorAlphaCode(id)
-        )
+        switchMap(({ id }) => this.paisService.getPaisPorAlphaCode(id)),
+        tap(console.log) //recibe el producto del observable y lo imprime en consola
+        // tap(resp => console.log(resp)) // lo mismo
       )
-      .subscribe(resp => {
-        console.log(resp);
-      })
+      .subscribe(alphaCodePaisParam => this.pais = alphaCodePaisParam)
 
     // this.activatedRoute.params
     //   .subscribe(({ id }) => {
