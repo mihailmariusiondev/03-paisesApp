a14791f1399ddf3337ee4b0515395b9b54905626
MariusMihailEnzo <marius.mihail@enzo.es>
2021-09-14 12:39:08 +0200

    106-tipado-peticion-RestCountries

--- paisesApp/src/app/pais/services/pais.service.ts
+++ paisesApp/src/app/pais/services/pais.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { Country } from '../interfaces/pais.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +14,9 @@ export class PaisService {
   constructor(private http: HttpClient) { }
 
   // Se retorna un Observable (preferente que el tipo de datos esté definido en una interfaz para devolver su tipo, y no un "any"
-  buscarPais(termino: string): Observable<any> {
+  buscarPais(termino: string): Observable<Country[]> {
+
     const url = `${this.apiUrl}name/${termino}`
-    return this.http.get(url)
-      .pipe(
-        catchError(err => [
-          of([]) // atrapa el error y retorna un arreglo vacío
-        ])
-      );
+    return this.http.get<Country[]>(url);      
   }
 }
