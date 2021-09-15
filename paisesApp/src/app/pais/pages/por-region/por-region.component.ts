969b1a4a2c42b0dc37b7596fd17f9868166ca4ee
MariusMihailEnzo <marius.mihail@enzo.es>
2021-09-15 11:38:20 +0200

    120-ngClass, class y [class.]

--- paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [`
+    button {
+      margin-right: 5px;
+    }
+  `
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania']
+  regionActiva: string = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+  activarRegion(region: string) {
+    this.regionActiva = region
+    console.log(region);
+
+    // TODO hacer el llamado al servicio
+
+    // FIXME hola
   }
+
 
 }
